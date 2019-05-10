9a4825ecd6a8a0bc886b08fad47072728c230606
TZsolt94 <blackcrazy00@gmail.com>
2019-05-10 13:59:06 +0200

    tesztek

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,84 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockTs1;
+  private TorpedoStore mockTs2;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockTs1 = mock(TorpedoStore.class);
+    mockTs2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTs1, mockTs2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockTs1.fire(1)).thenReturn(true);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockTs1, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(mockTs1.fire(1)).thenReturn(true);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
+    ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockTs2, times(1)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_haveTorpedo(){
+    // Arrange
+    when(mockTs1.isEmpty()).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    verify(mockTs1, times(1)).isEmpty();
+  }
+
+  @Test
+  public void fireTorpedo_dontHaveTorpedo(){
+    // Arrange
+    when(mockTs1.isEmpty()).thenReturn(true);
+    when(mockTs2.isEmpty()).thenReturn(true);
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    verify(mockTs1, times(0)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_dontHaveTorpedoInPrimary(){
+    // Arrange
+    when(mockTs1.isEmpty()).thenReturn(true);
+    when(mockTs2.isEmpty()).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    verify(mockTs2, times(1)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_tryFireTwice(){
+    // Arrange
+    when(mockTs1.isEmpty()).thenReturn(true);
+    when(mockTs2.isEmpty()).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTs2, times(2)).fire(1);
   }
 
 }
